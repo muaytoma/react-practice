6833c348f076c35812817480733d7e1991964c7a
Kitti Boonchan <kornkittig@gmail.com>
2016-08-28 11:35:25 +0700

    Fetch data from external API in ComponentDisMount callback, invoke once immediately after initial rendering occurs.

--- ui/components/Pages.js
+++ ui/components/Pages.js
@@ -1,7 +1,28 @@
 import React, {Component} from 'react'
+import fetch from 'isomorphic-fetch'
+import Page from './Page'
 
 export default class Pages extends Component {
+
+  state = {
+    pages: []
+  }
+
+  // componentWillMount() {
+  //   console.log('Pinto WillMount');
+  // }
+
+  componentDidMount() {
+    fetch('http://127.0.0.1:5000/api/v1/pages')
+      .then((response) => response.json())
+      .then((pages) => {
+        console.log('DisMount', pages);
+        this.setState({pages})
+      })
+  }
+
   render() {
+    console.log('Pages Render', this.state);
     return (
       // table is Global-css
       <table className='table'>
@@ -13,13 +34,16 @@ export default class Pages extends Component {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th>1</th>
-              <td>Title Page#1</td>
-              <td>
-                <a href='javascript:void(0)'>Show</a>
-              </td>
-            </tr>
+            {
+              this.state.pages.map((page) => {
+                return (
+                  <Page
+                  key={page.id}
+                  id={page.id}
+                  title={page.title} />
+                )
+              })
+            }
           </tbody>
       </table>
     )
