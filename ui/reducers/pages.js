2edc2e7e53170f47b6769dc7d2e7fc1e13a2f738
Kitti Boonchan <kornkittig@gmail.com>
2016-09-04 15:41:21 +0700

    Load page reducer, It bad way because reducer return [actions.new_payload] every load page( Show page) makes the State discard old list of pages data which already loaded.

--- ui/reducers/pages.js
+++ ui/reducers/pages.js
@@ -1,10 +1,26 @@
+import {ACTION_TYPES} from '../constants'
+
 const initialState = []
 
 export default (state=initialState, action) => {
   switch (action.type) {
-    case 'LOAD_PAGES_SUCCESS':
+    case ACTION_TYPES.LOAD_PAGES_SUCCESS:
       return action.payload
+    case ACTION_TYPES.LOAD_PAGE_SUCCESS:
+      /**
+       * symbol [] wapper because
+       * pages find callback function is works
+       
+       * This way makes all pages data lost.
+       */
+      return [action.payload]
     default:
       return state
   }
+}
+
+export const getPageById = (state, id) => {
+  return (
+    state.pages.find( page => page.id === +id || {title:'', content: ''})
+  )
 }
