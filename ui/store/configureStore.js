4800f6d2464ba0e677e435bf0c36a3bcfd117f84
Kitti Boonchan <kornkittig@gmail.com>
2016-09-04 14:02:44 +0700

    Redux Logger Middleware, The Actions and States activity.

--- ui/store/configureStore.js
+++ ui/store/configureStore.js
@@ -1,6 +1,7 @@
 import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {apiMiddleware} from 'redux-api-middleware'
+import createLogger from 'redux-logger'
 import rootReducer from '../reducers'
 
 /**
@@ -14,6 +15,13 @@ import rootReducer from '../reducers'
 
 export default () => {
   const middlewares = [thunk, apiMiddleware]
+
+  /**
+   * Redux-logger
+   */
+  if(process.env.NODE_ENV !== 'production')
+    middlewares.push(createLogger())
+
   const store = createStore(
     rootReducer,
     applyMiddleware(...middlewares)
