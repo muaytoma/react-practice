bbed9e6d926e83b1774006b4d5727d121422bb72
Kitti Boonchan <kornkittig@gmail.com>
2016-09-11 02:52:43 +0700

    Container get pagination data.

--- ui/containers/Griddle/Griddle.container.js
+++ ui/containers/Griddle/Griddle.container.js
@@ -1,53 +1,102 @@
 import React, {Component, PropTypes} from 'react'
 import { connect } from 'react-redux'
-import {GriddleComponent} from '../../components'
-import {loadParcels} from '../../actions'
+import {GriddleComponent, Loading} from '../../components'
+import Griddle from 'griddle-react'
+import {loadParcels, loadCustomers} from '../../actions'
 
 
 class GriddleContainer extends Component {
 
-  static propTypes = {
-    parcels: PropTypes.array.isRequired,
-    onLoadParcels: PropTypes.func.isRequired
+  // getInitialState
+  constructor(props) {
+    super(props)
+    this.state = {
+      "results": [],
+      "currentPage": 0,
+      "maxPages": 0,
+      "externalResultsPerPage": 10,
+      "externalSortColumn":null,
+      "externalSortAscending":true,
+      "isLoading": true
+    }
   }
 
-  getInitialState = () => ({
-    "results": [],
-    "currentPage": 0,
-    "maxPages": 0,
-    "externalResultsPerPage": 5,
-    "externalSortColumn":null,
-    "externalSortAscending":true
-  })
+  static propTypes = {
+    customers: PropTypes.array.isRequired,
+    onLoadCustomers: PropTypes.func.isRequired,
+    enableInfiniteScroll: PropTypes.bool
+  }
 
   // what page is currently viewed
-  setPage = (index) => {}
+  setPage = (index) => {
+    console.log('Set Page..', index);
+    index = index > this.state.maxPages ? this.state.maxPages : index < 1 ? 1 : index + 1;
+    this.onLoadCustomers(index);
+  }
 
-  onReloadParcels = () => {
-    this.props.onLoadParcels()
+  setPageSize = (size) => {
+  }
+
+  onLoadCustomers = (page = 1) => {
+    this.setState({currentPage: page-1, isLoading: true})
+    this.props.onLoadCustomers(page);
   }
 
   componentDidMount() {
-    this.onReloadParcels()
+    this.onLoadCustomers()
+  }
+
+  componentWillReceiveProps(props) {
+    let newState = {
+      maxPages: Math.round(200/10),
+      isLoading: false
+    };
+
+    // A bit more logic might be needed here. More or less, Griddle will
+    // only ever render what's being passed into it at any point.
+    if (props.enableInfiniteScroll && this.state.results) {
+      newState.results = this.state.results.concat(props.customers);
+    } else {
+      newState.results = props.customers;
+    }
+
+    this.setState(newState)
+  }
+
+  componentWillUpdate(props, state) {
+    console.log('Will Update..');
   }
 
   render() {
-    const initialState = this.getInitialState()
-    const {parcels} = this.props
+    const {customers, enableInfiniteScroll} = this.props
+    const columnsName = [
+      "id", "name"
+    ]
     return (
-      <GriddleComponent initialState={initialState} />
+      <Griddle useExternal={true} externalSetPage={this.setPage}
+        enableSort={false} columns={columnsName}
+        externalSetPageSize={this.setPageSize} externalMaxPage={this.state.maxPages}
+        externalChangeSort={function(){}} externalSetFilter={function(){}}
+        externalCurrentPage={this.state.currentPage} results={this.state.results}
+        tableClassName="table" resultsPerPage={this.state.externalResultsPerPage}
+        externalSortColumn={this.state.externalSortColumn} externalSortAscending={this.state.externalSortAscending}
+        externalLoadingComponent={Loading} externalIsLoading={this.state.isLoading}
+        enableInfiniteScroll={enableInfiniteScroll} bodyHeight={200} useFixedHeader={true} />
     )
   }
 
 }
 
+// getDefaultProps
+GriddleContainer.defaultProps = { enableInfiniteScroll: true }
+
 const mapStateToProps = (state) => ({
-  parcels: state.parcels
+  customers: state.customers
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  onLoadParcels() {
-    dispatch(loadParcels())
+  onLoadCustomers(page) {
+    dispatch(loadCustomers(page))
   }
 })
 
