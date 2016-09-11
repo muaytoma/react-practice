49b2795b9849661436caec05217f2e0236ea68f5
Kitti Boonchan <kornkittig@gmail.com>
2016-09-11 22:18:08 +0700

    Dev-serve proxy configuration

--- webpack.config.js
+++ webpack.config.js
@@ -73,6 +73,9 @@ module.exports = {
     proxy: {
       '/api/*': {
         target: 'http://127.0.0.1:5000'
+      },
+      '/admin/*': {
+        target: 'http://127.0.0.1:3000'
       }
     }
   }
