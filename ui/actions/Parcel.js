828dbc4f31f5a855f5c84a9da984a5661d006800
Kitti Boonchan <kornkittig@gmail.com>
2016-09-11 22:19:14 +0700

    Load parcels data action creator

--- ui/actions/Parcel.js
+++ ui/actions/Parcel.js
@@ -1,9 +1,9 @@
 import { CALL_API } from 'redux-api-middleware'
 import {API_ENDPOINT, ACTION_TYPES} from '../constants'
 
-export const loadParcels = (parcelSetId = 43) => ({
+export const loadParcels = (page = 0, parcelSetId = 48) => ({
   [CALL_API]: {
-    endpoint: `http://2stage-api.sendit.asia/admin/parcels/${parcelSetId}`,
+    endpoint: `${API_ENDPOINT.ADMIN_GET_PARCELS}/${parcelSetId}?page=${page}`,
     headers: {
       'Authorization': 'JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvd25lcklkIjoiMSIsIm93bmVyVHlwZSI6IkFETUlOIiwiaWF0IjoxNDYzNTY5OTYxLCJhdWQiOiJzZW5kaXQuYXNpYSIsImlzcyI6InNlbGYifQ.tIirQdhVDnfTEYmpsM_wVZEn5vK1lpXhuPlt1soxaNs'
     },
